e3d34ef6a75d1b8e4eb884a09990cfab27a2d19d
Mikhail Katychev <mkatych@gmail.com>
2020-09-19 16:25:08 -0500

    lesson 1.2 anchor

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,20 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let tokens = quote! {
+        impl #ident {
+            pub fn builder() {}
+        }
+    };
 
-    unimplemented!()
+    TokenStream::from(tokens)
+    // TokenStream::new()
 }
